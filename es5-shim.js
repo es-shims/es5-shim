ebd971fc3fbd68e7aaf303609b84981165d795bd
fschaefer <florian.schaefer@gmail.com>
2010-12-03 09:36:45 +0100

    Updated licensing and credit information.

--- es5-shim.js
+++ es5-shim.js
@@ -2,7 +2,7 @@
 // -- kriskowal Kris Kowal Copyright (C) 2009-2010 MIT License
 // -- tlrobinson Tom Robinson Copyright (C) 2009-2010 MIT License (Narwhal Project)
 // -- dantman Daniel Friesen Copyright(C) 2010 XXX No License Specified
-// -- fschaefer XXX Copyright (C) 2010 XXX No License Specified
+// -- fschaefer Florian Schäfer Copyright (C) 2010 MIT License
 
 /*!
     Copyright (c) 2009, 280 North Inc. http://280north.com/

67b09382316e45a9dc336e844eb8323158e08b5b
Sasha Koss <kossnocorp@gmail.com>
2011-02-28 12:46:10 +0600

    Added examples for Array.prototype.forEach

--- test/array.js
+++ test/array.js
@@ -16,3 +16,30 @@ describe('Array.isArray ( arg )', function () {
     });
 });
 
+describe('Array.prototype.forEach ( callbackFn [ , thisArg ] )', function () {
+
+    it('should be defined', function () {
+        assert([]).should(respondTo, 'forEach');
+    });
+
+    it('should pass three arguments: the value of the element, the index of the element, and the Array object being traversed', function () {
+        var testArray = [42];
+
+        testArray.forEach(function (element, index, array) {
+            assert(element).should(eql, 42);
+            assert(index).should(eql, 0);
+            assert(array).should(eql, testArray);
+        });
+    });
+
+    it('should executes the provided function (callback) once for each element present in the array', function () {
+        var testArray = [];
+
+        [2, 3, 4].forEach(function (element, index, array) {
+            testArray.push(element - 1);
+        });
+
+        assert(testArray).should(beSimilarTo, [1, 2, 3]);
+    });
+});
+

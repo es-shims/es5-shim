d17c101a6515df85f3792110996e53ab24a2555c
Irakli Gozalishvili <rfobic@gmail.com>
2011-03-18 15:37:32 +0100

    Adding `__proto__` to all the objects created by `Object.create` even if it's not supported by JS engine, cause this will guarantee proper behavior of `Object.getPrototypeOf` with such objects.

--- es5-shim.js
+++ es5-shim.js
@@ -396,18 +396,23 @@ if (!Object.getOwnPropertyNames) {
     };
 }
 
-// ES5 15.2.3.5 
+// ES5 15.2.3.5
 if (!Object.create) {
     Object.create = function create(prototype, properties) {
         var object;
         if (prototype === null) {
-            object = {"__proto__": null};
+            object = { "__proto__": null };
         } else {
             if (typeof prototype !== "object")
                 throw new TypeError("typeof prototype["+(typeof prototype)+"] != 'object'");
             var Type = function () {};
             Type.prototype = prototype;
             object = new Type();
+            // IE has no built-in implementation of `Object.getPrototypeOf`
+            // neither `__proto__`, but this manually setting `__proto__` will
+            // guarantee that `Object.getPrototypeOf` will work as expected with
+            // objects created using `Object.create`
+            object.__proto__ = prototype;
         }
         if (typeof properties !== "undefined")
             Object.defineProperties(object, properties);

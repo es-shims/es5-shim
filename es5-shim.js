26cc08d88eb5d9dbb31f2bbe954681c109cc3bbb
Kit Cambridge <kitcambridge@gmail.com>
2011-08-11 22:43:48 -0600

    Stylistic conformance for `Date#toISOString`.

--- es5-shim.js
+++ es5-shim.js
@@ -731,21 +731,23 @@ if (!Object.keys) {
 if (!Date.prototype.toISOString) {
     Date.prototype.toISOString = function toISOString() {
         var result, length, value;
-        if (!isFinite(this)) {
-          throw new RangeError;
-        }
-        // The date time string format is specified in 15.9.1.15.
-        result = [this.getUTCFullYear(), this.getUTCMonth() + 1, this.getUTCDate(), this.getUTCHours(), this.getUTCMinutes(), this.getUTCSeconds()];
+        if (!isFinite(this))
+            throw new RangeError;
+
+        // the date time string format is specified in 15.9.1.15.
+        result = [this.getUTCFullYear(), this.getUTCMonth() + 1, this.getUTCDate(),
+            this.getUTCHours(), this.getUTCMinutes(), this.getUTCSeconds()];
+
         length = result.length;
         while (length--) {
             value = result[length];
-            // Months, dates, hours, minutes, and seconds should have two digits.
-            if (value < 10) {
+            // pad months, days, hours, minutes, and seconds to have two digits.
+            if (value < 10)
                 result[length] = '0' + value;
-            }
         }
-        // Milliseconds should have three digits.
-        return result.slice(0, 3).join('-') + 'T' + result.slice(3).join(':') + '.' + ('000' + this.getUTCMilliseconds()).slice(-3) + 'Z';
+        // pad milliseconds to have three digits.
+        return result.slice(0, 3).join('-') + 'T' + result.slice(3).join(':') + '.' +
+            ('000' + this.getUTCMilliseconds()).slice(-3) + 'Z';
     }
 }
 

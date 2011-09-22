59aa58d2b418b5f9bc343c5243d488976ea6e697
Kris Kowal <kris.kowal@cixar.com>
2011-09-22 15:52:46 -0700

    Copyright, attribution, and license bylines need from the following contributors:
    -   @dantman
    -   @kossnocorp
    -   @bryanforbes
    -   @killdream
    -   @michaelficarra
    -   @sharkbrainguy
    -   @bbqsrc
    -   @iwyg
    -   @DomenicDenicola
    -   @xavierm02
    -   @Raynos
    -   @samsonjs
    -   @rwldrn
    -   @lexer
    
    Please edit your byline in ``es5-shim.js``.

--- es5-shim.js
+++ es5-shim.js
@@ -1,10 +1,23 @@
 // vim: ts=4 sts=4 sw=4 expandtab
-// -- kriskowal Kris Kowal Copyright (C) 2009-2010 MIT License
+// -- kriskowal Kris Kowal Copyright (C) 2009-2011 MIT License
 // -- tlrobinson Tom Robinson Copyright (C) 2009-2010 MIT License (Narwhal Project)
-// -- dantman Daniel Friesen Copyright(C) 2010 XXX No License Specified
+// -- dantman Daniel Friesen Copyright (C) 2010 XXX TODO License or CLA
 // -- fschaefer Florian Schäfer Copyright (C) 2010 MIT License
-// -- Irakli Gozalishvili Copyright (C) 2010 MIT License
+// -- Gozala Irakli Gozalishvili Copyright (C) 2010 MIT License
 // -- kitcambridge Kit Cambridge Copyright (C) 2011 MIT License
+// -- kossnocorp Sasha Koss XXX TODO License or CLA
+// -- bryanforbes Bryan Forbes XXX TODO License or CLA
+// -- killdream Quildreen Motta XXX TODO License or CLA
+// -- michaelficarra Michael Ficarra XXX TODO License or CLA
+// -- sharkbrainguy Gerard Paapu XXX TODO License or CLA
+// -- bbqsrc Brendan Molloy XXX TODO License or CLA
+// -- iwyg XXX TODO License or CLA
+// -- DomenicDenicola Domenic Denicola XXX TODO License or CLA
+// -- xavierm02 Montillet Xavier XXX TODO License or CLA
+// -- Raynos Raynos XXX TODO License or CLA
+// -- samsonjs Sami Samhuri XXX TODO License or CLA
+// -- rwldrn Rick Waldron XXX TODO License or CLA
+// -- lexer Alexey Zakharov XXX TODO License or CLA
 
 /*!
     Copyright (c) 2009, 280 North Inc. http://280north.com/

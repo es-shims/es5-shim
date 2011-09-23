7ac37b37483abdf74e24479cb07d44a378576fda
Kris Kowal <kris.kowal@cixar.com>
2011-09-22 17:08:35 -0700

    Merged @bryanforbes's improvements with toObject and consistent function type checking.

--- es5-shim.js
+++ es5-shim.js
@@ -177,12 +177,7 @@ var call = Function.prototype.call;
 var prototypeOfArray = Array.prototype;
 var prototypeOfObject = Object.prototype;
 var slice = prototypeOfArray.slice;
-// we *technically* don't have to bind toString because it
-// is a already a free variable when the global object
-// inherits from Object.prototype.  This will no longer be
-// true in modules of ES.next, but we'll be out of work by
-// then anyway.
-//var toString = prototypeOfObject.toString;
+var toString = call.bind(prototypeOfObject.toString);
 var owns = call.bind(prototypeOfObject.hasOwnProperty);
 
 // If JS engine supports accessors creating shortcuts.
@@ -206,22 +201,35 @@ if ((supportsAccessors = owns(prototypeOfObject, "__defineGetter__"))) {
 // ES5 15.4.3.2
 if (!Array.isArray) {
     Array.isArray = function isArray(obj) {
-        return toString.call(obj) == "[object Array]";
+        return toString(obj) == "[object Array]";
     };
 }
 
+// The IsCallable() check in the Array functions
+// has been replaced with a strict check on the
+// internal class of the object to trap cases where
+// the provided function was actually a regular
+// expression literal, which in V8 and
+// JavaScriptCore is a typeof "function".  Only in
+// V8 are regular expression literals permitted as
+// reduce parameters, so it is desirable in the
+// general case for the shim to match the more
+// strict and common behavior of rejecting regular
+// expressions.
+
 // ES5 15.4.4.18
 // https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/array/foreach
-if (!prototypeOfArray.forEach) {
-    prototypeOfArray.forEach = function forEach(fun /*, thisp*/) {
-        var self = Object(this),
+if (!Array.prototype.forEach) {
+    Array.prototype.forEach = function forEach(fun /*, thisp*/) {
+        var self = toObject(this),
             thisp = arguments[1],
             i = 0,
             length = self.length >>> 0;
 
         // If no callback function or if callback is not a callable function
-        if (typeof fun != "function")
+        if (toString(fun) != "[object Function]") {
             throw new TypeError(); // TODO message
+        }
 
         while (i < length) {
             if (i in self) {
@@ -236,14 +244,18 @@ if (!prototypeOfArray.forEach) {
 
 // ES5 15.4.4.19
 // https://developer.mozilla.org/en/Core_JavaScript_1.5_Reference/Objects/Array/map
-if (!prototypeOfArray.map) {
-    prototypeOfArray.map = function map(fun /*, thisp*/) {
-        var self = Object(this);
-        var length = self.length >>> 0;
-        if (typeof fun != "function")
+if (!Array.prototype.map) {
+    Array.prototype.map = function map(fun /*, thisp*/) {
+        var self = toObject(this),
+            length = self.length >>> 0,
+            result = Array(length),
+            thisp = arguments[1];
+
+        // If no callback function or if callback is not a callable function
+        if (toString(fun) != "[object Function]") {
             throw new TypeError(); // TODO message
-        var result = new Array(length);
-        var thisp = arguments[1];
+        }
+
         for (var i = 0; i < length; i++) {
             if (i in self)
                 result[i] = fun.call(thisp, self[i], i, self);
@@ -253,31 +265,38 @@ if (!prototypeOfArray.map) {
 }
 
 // ES5 15.4.4.20
-if (!prototypeOfArray.filter) {
-    prototypeOfArray.filter = function filter(fun /*, thisp */) {
-        var self = Object(this);
-        var length = self.length >>> 0;
-        if (typeof fun != "function")
+if (!Array.prototype.filter) {
+    Array.prototype.filter = function filter(fun /*, thisp */) {
+        var self = toObject(this),
+            length = self.length >>> 0,
+            result = [],
+            thisp = arguments[1];
+
+        // If no callback function or if callback is not a callable function
+        if (toString(fun) != "[object Function]") {
             throw new TypeError(); // TODO message
-        var result = [];
-        var thisp = arguments[1];
-        for (var i = 0; i < length; i++)
+        }
+
+        for (var i = 0; i < length; i++) {
             if (i in self && fun.call(thisp, self[i], i, self))
                 result.push(self[i]);
+        }
         return result;
     };
 }
 
 // ES5 15.4.4.16
-if (!prototypeOfArray.every) {
-    prototypeOfArray.every = function every(fun /*, thisp */) {
-        if (this === void 0 || this === null)
+if (!Array.prototype.every) {
+    Array.prototype.every = function every(fun /*, thisp */) {
+        var self = toObject(this),
+            length = self.length >>> 0,
+            thisp = arguments[1];
+
+        // If no callback function or if callback is not a callable function
+        if (toString(fun) != "[object Function]") {
             throw new TypeError(); // TODO message
-        if (typeof fun != "function")
-            throw new TypeError(); // TODO message
-        var self = Object(this);
-        var length = self.length >>> 0;
-        var thisp = arguments[1];
+        }
+
         for (var i = 0; i < length; i++) {
             if (i in self && !fun.call(thisp, self[i], i, self))
                 return false;
@@ -288,15 +307,17 @@ if (!prototypeOfArray.every) {
 
 // ES5 15.4.4.17
 // https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/some
-if (!prototypeOfArray.some) {
-    prototypeOfArray.some = function some(fun /*, thisp */) {
-        if (this === void 0 || this === null)
+if (!Array.prototype.some) {
+    Array.prototype.some = function some(fun /*, thisp */) {
+        var self = toObject(this),
+            length = self.length >>> 0,
+            thisp = arguments[1];
+
+        // If no callback function or if callback is not a callable function
+        if (toString(fun) != "[object Function]") {
             throw new TypeError(); // TODO message
-        if (typeof fun != "function")
-            throw new TypeError(); // TODO message
-        var self = Object(this);
-        var length = self.length >>> 0;
-        var thisp = arguments[1];
+        }
+
         for (var i = 0; i < length; i++) {
             if (i in self && fun.call(thisp, self[i], i, self))
                 return true;
@@ -307,17 +328,15 @@ if (!prototypeOfArray.some) {
 
 // ES5 15.4.4.21
 // https://developer.mozilla.org/en/Core_JavaScript_1.5_Reference/Objects/Array/reduce
-if (!prototypeOfArray.reduce) {
-    prototypeOfArray.reduce = function reduce(fun /*, initial*/) {
-        var self = Object(this);
-        var length = self.length >>> 0;
-        // We diverge from the spec here by testing that fun.[[Class]] is
-        // "Function" instead of IsCallable(fun) as specified in 15.4.4.21 step 4
-        // This is to ensure that shims behave consistently even in the
-        // presence of a bug in minority browsers where regular expressions are
-        // callable.
-        if (toString.call(fun) != "[object Function]")
+if (!Array.prototype.reduce) {
+    Array.prototype.reduce = function reduce(fun /*, initial*/) {
+        var self = toObject(this),
+            length = self.length >>> 0;
+
+        // If no callback function or if callback is not a callable function
+        if (toString(fun) != "[object Function]") {
             throw new TypeError(); // TODO message
+        }
 
         // no value to return if no initial value and an empty array
         if (!length && arguments.length == 1)
@@ -351,15 +370,16 @@ if (!prototypeOfArray.reduce) {
 
 // ES5 15.4.4.22
 // https://developer.mozilla.org/en/Core_JavaScript_1.5_Reference/Objects/Array/reduceRight
-if (!prototypeOfArray.reduceRight) {
-    prototypeOfArray.reduceRight = function reduceRight(fun /*, initial*/) {
-        var self = Object(this);
-        var length = self.length >>> 0;
-        // We diverge from the spec right here by testing that fun.[[Class]] is
-        // "Function" instead of IsCallable(fun) as specified in 15.4.4.22 step 4
-        // TODO: explain why this decision was made
-        if (toString.call(fun) != "[object Function]")
+if (!Array.prototype.reduceRight) {
+    Array.prototype.reduceRight = function reduceRight(fun /*, initial*/) {
+        var self = toObject(this),
+            length = self.length >>> 0;
+
+        // If no callback function or if callback is not a callable function
+        if (toString(fun) != "[object Function]") {
             throw new TypeError(); // TODO message
+        }
+
         // no value to return if no initial value, empty array
         if (!length && arguments.length == 1)
             throw new TypeError(); // TODO message
@@ -391,17 +411,18 @@ if (!prototypeOfArray.reduceRight) {
 
 // ES5 15.4.4.14
 // https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/indexOf
-if (!prototypeOfArray.indexOf) {
-    prototypeOfArray.indexOf = function indexOf(sought /*, fromIndex */ ) {
-        if (this === void 0 || this === null)
-            throw new TypeError(); // TODO message
-        var self = Object(this);
-        var length = self.length >>> 0;
+if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function indexOf(sought /*, fromIndex */ ) {
+        var self = toObject(this),
+            length = self.length >>> 0;
+
         if (!length)
             return -1;
+
         var i = 0;
         if (arguments.length > 1)
             i = toInteger(arguments[1]);
+
         // handle negative indices
         i = i >= 0 ? i : length - Math.abs(i);
         for (; i < length; i++) {
@@ -414,12 +435,11 @@ if (!prototypeOfArray.indexOf) {
 }
 
 // ES5 15.4.4.15
-if (!prototypeOfArray.lastIndexOf) {
-    prototypeOfArray.lastIndexOf = function lastIndexOf(sought /*, fromIndex */) {
-        if (this === void 0 || this === null)
-            throw new TypeError(); // TODO message
-        var self = Object(this);
-        var length = self.length >>> 0;
+if (!Array.prototype.lastIndexOf) {
+    Array.prototype.lastIndexOf = function lastIndexOf(sought /*, fromIndex */) {
+        var self = toObject(this),
+            length = self.length >>> 0;
+
         if (!length)
             return -1;
         var i = length - 1;
@@ -713,7 +733,7 @@ if (!Object.isExtensible) {
     Object.isExtensible = function isExtensible(object) {
         // 1. If Type(O) is not Object throw a TypeError exception.
         if (Object(object) === object) {
-            throw new TypeError();
+            throw new TypeError(); // TODO message
         }
         // 2. Return the Boolean value of the [[Extensible]] internal property of O.
         var name = '';
@@ -985,4 +1005,17 @@ var toInteger = function (n) {
     return n;
 };
 
+var prepareString = "a"[0] != "a",
+    // ES5 9.9
+    toObject = function (o) {
+        if (o == null) { // this matches both null and undefined
+            throw new TypeError(); // TODO message
+        }
+        // If the implementation doesn't support by-index access of
+        // string characters (ex. IE < 7), split the string
+        if (prepareString && typeof o == "string" && o) {
+            return o.split("");
+        }
+        return Object(o);
+    };
 });

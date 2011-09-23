99fd16aa886b97c28a902ed04968314b70fdc57f
Rick Waldron <waldron.rick@gmail.com>
2011-09-22 20:32:04 -0400

    Adds rwldrn, Rick Waldron MIT Lic.

--- es5-shim.js
+++ es5-shim.js
@@ -16,7 +16,7 @@
 // -- xavierm02 Montillet Xavier XXX TODO License or CLA
 // -- Raynos Raynos XXX TODO License or CLA
 // -- samsonjs Sami Samhuri XXX TODO License or CLA
-// -- rwldrn Rick Waldron XXX TODO License or CLA
+// -- rwldrn Rick Waldron Copyright (C) 2011 MIT License
 // -- lexer Alexey Zakharov XXX TODO License or CLA
 
 /*!

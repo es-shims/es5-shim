8cbb6fea6cc110370a5f8df3b17a00bcad891965
Raynos <raynos2@gmail.com>
2011-11-07 17:47:35 +0000

    migrated toISOString test from Qunit test suite

--- tests/spec/s-date.js
+++ tests/spec/s-date.js
@@ -14,12 +14,21 @@ describe('Date', function () {
             expect(Date.parse('0001-01-01T00:00:00Z')).toBe(62135596800000);
             expect(Date.parse('+275760-09-13T00:00:00.000Z')).toBe(8.64e15);
             expect(Date.parse('+033658-09-27T01:46:40.000Z')).toBe(1e15);
-            // Unit test fails what do >_<
+            // Unit test fails here. Chrome 14 agrees this unit tests should be correct
             expect(Date.parse('-000001-01-01T00:00:00Z')).toBe(-62198755200000);
             expect(Date.parse('+002009-12-15T00:00:00Z')).toBe(1260835200000);
 
         });
 
+    });
+
+    describe("toISOString", function () {
+        // TODO: write the rest of the test.
+       
+        it('should support extended years', function () {
+            expect(new Date(-62198755200000).toISOString().indexOf('-000001-01-01')).toBe(0);
+            expect(new Date(8.64e15).toISOString().indexOf('+275760-09-13')).toBe(0);
+        });
     });
 
 });

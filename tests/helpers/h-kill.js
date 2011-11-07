73e92e0bb2cfd95e7bcf628d747a3016944f7a3c
Raynos <raynos2@gmail.com>
2011-11-07 17:14:36 +0000

    Updated helper to work with non prototype objects

--- tests/helpers/h-kill.js
+++ tests/helpers/h-kill.js
@@ -10,20 +10,24 @@ var HLP = (function() {
         if(!_class.originals)
             _class.originals = {};
         methods.forEach(function(obj) {
-            _class.originals[obj] = _class.prototype[obj];
-            _class.prototype[obj] = undefined;
+            _class.originals[obj] = _class[obj];
+            _class[obj] = undefined;
         });
     };
     return { kill: kill };
 }());
 
-HLP.kill(Function, [
+HLP.kill(Function.prototype, [
     'bind'
 ]);
 
-HLP.kill(Array, [
+HLP.kill(Array.prototype, [
     'forEach', 'some', 'every', 
     'indexOf', 'lastIndexOf', 
     'map', 'filter', 
     'reduce', 'reduceRight'
+]);
+
+HLP.kill(Array, [
+    'isArray'
 ]);

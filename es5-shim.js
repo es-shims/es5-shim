9ea8313bcaa5537e3f3d6490dee92a5de44258b5
Yaffle <vic99999@yandex.ru>
2012-04-09 12:01:20 +0600

    Date.parse

--- es5-shim.js
+++ es5-shim.js
@@ -984,9 +984,9 @@ if (!Date.parse || "Date.parse is buggy") {
 
         var monthes = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365];
 
-        // Returns count of leap years before "year" since 0 CE
-        function leapYears(year) {
-            return Math.ceil(year / 4) - Math.ceil(year / 100) + Math.ceil(year / 400);
+        function dayFromMonth(year, month) {
+            var t = month > 1 ? 1 : 0;
+            return monthes[month] + Math.floor((year - 1969 + t) / 4) - Math.floor((year - 1901 + t) / 100) + Math.floor((year - 1601 + t) / 400) + 365 * (year - 1970);
         }
 
         // Copy any custom methods a 3rd party library may have added
@@ -1008,33 +1008,25 @@ if (!Date.parse || "Date.parse is buggy") {
                 // provide default values if necessary
                 // parse the UTC offset component
                 var year = Number(match[1]),
-                    month = Number(match[2] || 1),
-                    day = Number(match[3] || 1),
+                    month = Number(match[2] || 1) - 1,
+                    day = Number(match[3] || 1) - 1,
                     hour = Number(match[4] || 0),
                     minute = Number(match[5] || 0),
                     second = Number(match[6] || 0),
                     millisecond = Number(match[7] || 0),
                     // When time zone is missed, local offset should be used (ES 5.1 bug)
                     // see https://bugs.ecmascript.org/show_bug.cgi?id=112
-                    offset = match[8] ? 0 : Number(new Date(1970, 0)),
+                    offset = !match[4] || match[8] ? 0 : Number(new Date(1970, 0)),
                     signOffset = match[9] === "-" ? 1 : -1,
                     hourOffset = Number(match[10] || 0),
-                    minuteOffset = Number(match[11] || 0),    
-                    leapYears0 = leapYears(year),
-                    leapYears1 = leapYears(year + 1),
+                    minuteOffset = Number(match[11] || 0),
                     result;
-                if (hour < (minute > 0 || second > 0 || millisecond > 0 ? 24 : 25) &&
-                    minute < 60 &&
-                    second < 60 &&
-                    millisecond < 1000 &&
-                    hourOffset < 24 && // detect invalid offsets
-                    minuteOffset < 60 &&
-                    month > 0 &&
-                    month < 13 &&
-                    day > 0 &&
-                    day < (1 + monthes[month] - monthes[month - 1] + (month === 2 ? leapYears1 - leapYears0 : 0))) {
-                    result = 365 * (year - 1970) + (month > 2 ? leapYears1 : leapYears0) - leapYears(1970) + monthes[month - 1] + day - 1;
-                    result = (((result * 24 + hour + hourOffset * signOffset) * 60 + minute + minuteOffset * signOffset) * 60 + second) * 1000 + millisecond + offset;
+                if (hour < (minute > 0 || second > 0 || millisecond > 0 ? 24 : 25) && 
+                    minute < 60 && second < 60 && millisecond < 1000 && 
+                    month > -1 && month < 12 && hourOffset < 24 && minuteOffset < 60 && // detect invalid offsets
+                    day > -1 && day < dayFromMonth(year, month + 1) - dayFromMonth(year, month)) {
+                    result = ((dayFromMonth(year, month) + day) * 24 + hour + hourOffset * signOffset) * 60;
+                    result = ((result + minute + minuteOffset * signOffset) * 60 + second) * 1000 + millisecond + offset;
                     if (-8.64e15 <= result && result <= 8.64e15) {
                         return result;
                     }

c1d743e1f9de7d068a4ebf367f5d06cedb012f82
Tyler Akins <fidian@rumkin.com>
2013-01-29 09:16:01 -0600

    Correcting tabs to match spaces

--- tests/spec/s-number.js
+++ tests/spec/s-number.js
@@ -3,7 +3,7 @@ describe('Number', function () {
     
     describe('toFixed', function () {
         it('should convert numbers correctly', function () {
-			expect((0.00008).toFixed(3)).toBe('0.000');
+            expect((0.00008).toFixed(3)).toBe('0.000');
             expect((0.9).toFixed(0)).toBe('1');
             expect((1.255).toFixed(2)).toBe('1.25');
             expect((1843654265.0774949).toFixed(5)).toBe('1843654265.07749');

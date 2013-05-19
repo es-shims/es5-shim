00a8838f2b7f22e872cebe542013271fdd9b7a1e
Tyler Akins <fidian@rumkin.com>
2013-05-19 17:01:12 -0500

    Adding Steven Levithan's String.prototype.split for RegExp
    I asked and received his blessing:
    
    Tyler, thanks for offering to take this up! I’d be more than happy to see my code for this shared more broadly, and/or improved. My split shim and tests are MIT licensed, like es5-shim. By the way, Kris (author of es5-shim) brought this up before, but no one has done the work to see this through, like you’ve offered to do. See https://github.com/kriskowal/es5-shim/issues/103
    
     /Steven

--- es5-shim.js
+++ es5-shim.js
@@ -1105,6 +1105,100 @@ if("0".split(void 0, 0).length) {
     }
 }
 
+// ES5 15.5.4.14
+// http://es5.github.com/#x15.5.4.14
+// [bugfix, IE lt 9, firefox 4, Konqueror, Opera, obscure browsers]
+// Many browsers do not split properly with regular expressions or they
+// do not perform the split correctly under obscure conditions.
+// See http://blog.stevenlevithan.com/archives/cross-browser-split
+// I've tested in many browsers and this seems to cover the deviant ones:
+//    'ab'.split(/(?:ab)*/) should be ["", ""], not [""]
+//    '.'.split(/(.?)(.?)/) should be ["", ".", "", ""], not ["", ""]
+//    'tesst'.split(/(s)*/) should be ["t", undefined, "e", "s", "t"], not
+//       [undefined, "t", undefined, "e", ...]
+//    ''.split(/.?/) should be [], not [""]
+//    '.'.split(/()()/) should be ["."], not ["", "", "."]
+
+if ('ab'.split(/(?:ab)*/).length !== 2 || 
+	'.'.split(/(.?)(.?)/).length !== 4 ||
+	'tesst'.split(/(s)*/)[1] === "t" ||
+	''.split(/.?/).length === 0 ||
+	'.'.split(/()()/).length > 1) {
+	(function (undef) {
+    var nativeSplit = String.prototype.split,
+        compliantExecNpcg = /()??/.exec("")[1] === undef; // NPCG: nonparticipating capturing group
+
+		String.prototype.split = function (str, separator, limit) {
+			// If `separator` is not a regex, use `nativeSplit`
+			if (Object.prototype.toString.call(separator) !== "[object RegExp]") {
+				return nativeSplit.apply(this, arguments);
+			}
+
+			var output = [],
+				flags = (separator.ignoreCase ? "i" : "") +
+						(separator.multiline  ? "m" : "") +
+						(separator.extended   ? "x" : "") + // Proposed for ES6
+						(separator.sticky     ? "y" : ""), // Firefox 3+
+				lastLastIndex = 0,
+				// Make `global` and avoid `lastIndex` issues by working with a copy
+				separator = new RegExp(separator.source, flags + "g"),
+				separator2, match, lastIndex, lastLength;
+			str += ""; // Type-convert
+			if (!compliantExecNpcg) {
+				// Doesn't need flags gy, but they don't hurt
+				separator2 = new RegExp("^" + separator.source + "$(?!\\s)", flags);
+			}
+			/* Values for `limit`, per the spec:
+			 * If undefined: 4294967295 // Math.pow(2, 32) - 1
+			 * If 0, Infinity, or NaN: 0
+			 * If positive number: limit = Math.floor(limit); if (limit > 4294967295) limit -= 4294967296;
+			 * If negative number: 4294967296 - Math.floor(Math.abs(limit))
+			 * If other: Type-convert, then use the above rules
+			 */
+			limit = limit === undef ?
+				-1 >>> 0 : // Math.pow(2, 32) - 1
+				limit >>> 0; // ToUint32(limit)
+			while (match = separator.exec(str)) {
+				// `separator.lastIndex` is not reliable cross-browser
+				lastIndex = match.index + match[0].length;
+				if (lastIndex > lastLastIndex) {
+					output.push(str.slice(lastLastIndex, match.index));
+					// Fix browsers whose `exec` methods don't consistently return `undefined` for
+					// nonparticipating capturing groups
+					if (!compliantExecNpcg && match.length > 1) {
+						match[0].replace(separator2, function () {
+							for (var i = 1; i < arguments.length - 2; i++) {
+								if (arguments[i] === undef) {
+									match[i] = undef;
+								}
+							}
+						});
+					}
+					if (match.length > 1 && match.index < str.length) {
+						Array.prototype.push.apply(output, match.slice(1));
+					}
+					lastLength = match[0].length;
+					lastLastIndex = lastIndex;
+					if (output.length >= limit) {
+						break;
+					}
+				}
+				if (separator.lastIndex === match.index) {
+					separator.lastIndex++; // Avoid an infinite loop
+				}
+			}
+			if (lastLastIndex === str.length) {
+				if (lastLength || !separator.test("")) {
+					output.push("");
+				}
+			} else {
+				output.push(str.slice(lastLastIndex));
+			}
+			return output.length > limit ? output.slice(0, limit) : output;
+		};
+	}());
+}
+
 // ECMA-262, 3rd B.2.3
 // Note an ECMAScript standart, although ECMAScript 3rd Edition has a
 // non-normative section suggesting uniform semantics and it should be

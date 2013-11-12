bb53602ea79dc920e542c41537bb8c8ecccde93f
Brian Di Palma <offler@gmail.com>
2013-11-12 09:27:51 +0000

    Added comment next to semicolon to explain their presence.

--- es5-sham.js
+++ es5-sham.js
@@ -1,8 +1,9 @@
 // Copyright 2009-2012 by contributors, MIT License
 // vim: ts=4 sts=4 sw=4 expandtab
 
-// Module systems magic dance
+//Add semicolon to prevent IIFE from being passed as argument to concated code.
 ;
+// Module systems magic dance
 (function (definition) {
     // RequireJS
     if (typeof define == "function") {

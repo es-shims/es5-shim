4dd33d7d19c9cd555b051065efee76f06096da13
Jordan Harband <ljharb@gmail.com>
2013-12-30 15:14:42 -0800

    Support standard whitespace trimming.

--- es5-shim.js
+++ es5-shim.js
@@ -1262,7 +1262,7 @@ if ("".substr && "0b".substr(-1) !== "b") {
 }
 
 // ES5 15.5.4.20
-// http://es5.github.com/#x15.5.4.20
+// whitespace from: http://es5.github.io/#x15.5.4.20
 var ws = "\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u180E\u2000\u2001\u2002\u2003" +
     "\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028" +
     "\u2029\uFEFF";
@@ -1283,7 +1283,7 @@ if (!String.prototype.trim || ws.trim()) {
 }
 
 // ES-5 15.1.2.2
-if (parseInt('08') !== 8) {
+if (parseInt(ws + '08') !== 8 || parseInt(ws + '0x16') !== 22) {
     parseInt = (function (origParseInt) {
         var hexRegex = /^0[xX]/;
         return function parseIntES5(str, radix) {

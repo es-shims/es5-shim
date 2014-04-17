6ac43d0a31f09ba0768f460eb2608cffcfb6165a
Jordan Harband <ljharb@gmail.com>
2014-04-16 21:32:27 -0700

    Ensure Date.parse supports extended years.

--- es5-shim.js
+++ es5-shim.js
@@ -849,7 +849,8 @@ if (!dateToJSONIsSupported) {
 // http://es5.github.com/#x15.9.4.2
 // based on work shared by Daniel Friesen (dantman)
 // http://gist.github.com/303249
-if (!Date.parse || isNaN(Date.parse("2000-01-01T00:00:00.000Z"))) {
+var supportsExtendedYears = Date.parse('+033658-09-27T01:46:40.000Z') === 1e15;
+if (!Date.parse || isNaN(Date.parse("2000-01-01T00:00:00.000Z")) || !supportsExtendedYears) {
     // XXX global assignment won't work in embeddings that use
     // an alternate object for the context.
     Date = (function(NativeDate) {

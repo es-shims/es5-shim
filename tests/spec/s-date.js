ab6a7fb3febe214e7f1ede6501b418743c2675f1
Jordan Harband <ljharb@gmail.com>
2014-05-14 09:57:27 -0700

    Better failure message for this Date.now test

--- tests/spec/s-date.js
+++ tests/spec/s-date.js
@@ -2,7 +2,7 @@ describe('Date', function () {
 
     describe('now', function () {
         it('should be the current time', function () {
-            expect(Date.now() === new Date().getTime()).toBe(true);
+            expect(Date.now()).toBe(new Date().getTime());
         });
     });
 

519e8326a2ebf73f37a1902d21fa0dd81665c8ac
Jordan Harband <ljharb@gmail.com>
2015-04-11 12:51:31 -0700

    Update copyright year.

--- es5-sham.js
+++ es5-sham.js
@@ -1,6 +1,6 @@
 /*!
  * https://github.com/es-shims/es5-shim
- * @license es5-shim Copyright 2009-2014 by contributors, MIT License
+ * @license es5-shim Copyright 2009-2015 by contributors, MIT License
  * see https://github.com/es-shims/es5-shim/blob/master/LICENSE
  */
 

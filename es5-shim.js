7f38f764befe94f20824d2ffcdfcd6f871921224
Joël Galeran <Jolg42@users.noreply.github.com>
2015-11-13 11:42:53 +0100

    [Docs] Fix broken UMD links.
    Closes #344.

--- es5-shim.js
+++ es5-shim.js
@@ -10,7 +10,7 @@
 ;
 
 // UMD (Universal Module Definition)
-// see https://github.com/umdjs/umd/blob/master/returnExports.js
+// see https://github.com/umdjs/umd/blob/master/templates/returnExports.js
 (function (root, factory) {
     'use strict';
 

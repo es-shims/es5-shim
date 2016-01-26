d78a10734c459d1d88a0785fa697d6c33e93c43c
Jordan Harband <ljharb@gmail.com>
2016-01-26 11:09:44 -0800

    [Fix] use `Array#slice`, not `String#slice`, on `String#split` output.
    Fixes #380.

--- es5-shim.js
+++ es5-shim.js
@@ -1513,7 +1513,7 @@ if (
             } else {
                 array_push.call(output, strSlice(string, lastLastIndex));
             }
-            return output.length > splitLimit ? strSlice(output, 0, splitLimit) : output;
+            return output.length > splitLimit ? arraySlice(output, 0, splitLimit) : output;
         };
     }());
 
